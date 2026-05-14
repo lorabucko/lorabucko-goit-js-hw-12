528a7565b4d78498bb98ef244c88994deb9b00ed
lora_bucko <lorabucko@gmail.com>
2026-05-14 17:49:25 +0200

    Add pagination to getImagesByQuery function

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 
 const API_KEY = '55760318-43e5d4b90f7f21f4193a7db8f';
 const BASE_URL = 'https://pixabay.com/api/';
+const PER_PAGE = 15;
 
-export async function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page) {
   const response = await axios.get(BASE_URL, {
     params: {
       key: API_KEY,
       q: query,
+      page,
+      per_page: PER_PAGE,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
@@ -16,3 +19,5 @@ export async function getImagesByQuery(query) {
 
   return response.data;
 }
+
+export { PER_PAGE };
